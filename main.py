b85b3f5dada47eb8df5629934bd44ea147a08f7d
Lidiya <lidiiarusina@yandex.ru>
2024-12-27 21:23:35 +0300

    Second commit - Random Circles

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 import sys
 from random import randint
 
-from PyQt6 import uic
+from Ui import Ui_MainWindow
 from PyQt6.QtGui import QColor, QPainter
 from PyQt6.QtWidgets import QApplication
 from PyQt6.QtWidgets import QMainWindow
 
 
-class MyWidget(QMainWindow):
+class MyWidget(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('Ui.ui', self)
+        self.setupUi(self)
         self.do_paint = False
         self.pushButton.clicked.connect(self.create_circle)
 
@@ -22,7 +22,7 @@ class MyWidget(QMainWindow):
             qp.end()
 
     def draw(self, qp):
-        qp.setBrush(QColor(255, 255, 0))
+        qp.setBrush(QColor(randint(0, 255), randint(0, 255), randint(0, 255)))
         r = randint(0, 250)
         qp.drawEllipse(randint(0, 600 - r), randint(0, 600 - r), r, r)
         self.do_paint = False
